2759e87a34c1896d9f26d25716eb28494f8b5596
valentin fechter <valentin@valentins-MacBook-Air.local>
2026-02-17 00:04:42 +0100

    add parsing.py, config.txt, main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+import parsing
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("We want only one argument (argument: config.txt)")
+        sys.exit(-1)
+    result = parsing.parsing_config_data(sys.argv[1])
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
---
+++ parsing.py
@@ -0,0 +1,85 @@
+from typing import Any
+
+
+def parsing_config_data(file_name: str) -> dict[str, Any] | None:
+    """Parses the configuration file and returns a dictionary of settings."""
+    if file_name != "config.txt":
+        print("Invalid filename. Expected 'config.txt'.")
+        return None
+
+    config_data = {}
+    try:
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.rstrip("\n")
+                if not line or line.startswith("#"):
+                    continue
+                if "=" not in line:
+                    print("Invalid format. Missing '=' in line: " + line)
+                    return None
+                if " " in line:
+                    print(f"Format Error: Spaces are not allowed in '{line}'")
+                    return None
+                (key, _, value) = line.partition("=")
+                try:
+                    match key:
+                        case "WIDTH":
+                            config_data["width"] = int(value)
+                        case "HEIGHT":
+                            config_data["height"] = int(value)
+                        case "ENTRY" | "EXIT":
+                            parts = value.split(",")
+                            if len(parts) != 2:
+                                print(
+                                    f"Error: {key} must contain exactly two coordinates (x,y)."
+                                )
+                                return None
+                            coords = tuple(int(n) for n in parts)
+                            config_data[key.lower()] = coords
+                        case "OUTPUT_FILE":
+                            if value != "maze.txt":
+                                print("The OUTPUT_FILE must be 'maze.txt'.")
+                                return None
+                            config_data["output_file"] = value
+                        case "PERFECT":
+                            if value.lower() not in ["true", "false"]:
+                                print("The PERFECT value must be 'true' or 'false'.")
+                                return None
+                            config_data["perfect"] = value.lower() == "true"
+                        case _:
+                            print(f"Error: Unknown key '{key}' found in config.txt.")
+                            return None
+                except ValueError as e:
+                    print(f"Error: {e}")
+    except FileNotFoundError:
+        print(f"Error: The file '{file_name}' was not found in the current directory.")
+        return None
+    except PermissionError:
+        print(f"Error: You do not have permission to read '{file_name}'.")
+        return None
+
+    required_keys = ["width", "height", "entry", "exit", "perfect", "output_file"]
+    for req in required_keys:
+        if req not in config_data:
+            print(f"Error: Missing required configuration key: {req.upper()}")
+            return None
+
+    width = config_data.get("width")
+    height = config_data.get("height")
+
+    if width is None or height is None:
+        print("Error: WIDTH and HEIGHT must be defined in config.txt")
+        return None
+
+    if width <= 1 or height <= 1:
+        print("Error: height or width must be greater than 1")
+        return None
+
+    for key in ["entry", "exit"]:
+        if key in config_data and width and height:
+            x, y = config_data[key]
+            if not (0 <= x < width and 0 <= y < height):
+                print(f"Error: {key.upper()} {x, y} is out of maze boundaries.")
+                return None
+    return config_data
