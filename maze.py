873b4abefa4a7a5205ffe000dead1c5265073657
Valentin Fechter <vafechte@k1r1p5.42mulhouse.fr>
2026-02-18 17:39:27 +0100

    add comment to get_random_valid_cell

--- maze.py
+++ maze.py
@@ -17,28 +17,31 @@ class Maze:
         width = self.width
         height = self.height
 
+        # the coordinates of the cells around the current cell.
         cell_coords = [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]
-        random.shuffle(cell_coords)
+        random.shuffle(cell_coords) # shuffle cell_coords to randomize the array
 
+        # get a cell
         for _ in range(0, 4):
             x = cell_coords[0][0]
             y = cell_coords[0][1]
             if not (0 <= x < width and 0 <= y
-                    < height):
+                    < height): # if out of range, we remove the current cell
                 cell_coords.remove((cell_coords[0][0], cell_coords[0][1]))
-            elif self.maze[x][y].has_visited is True:
+            elif self.maze[x][y].has_visited is True: # if the cell has already been visited, we remove the cell
                 cell_coords.remove((cell_coords[0][0], cell_coords[0][1]))
             else:
                 break
 
+        # if there are no cells available then we return None
         if (len(cell_coords) == 0):
             return None
 
-        x = cell_coords[0][0]
-        y = cell_coords[0][1]
+        x = cell_coords[0][0] # we get x coordinate
+        y = cell_coords[0][1] # we get y coordinate
 
-        cell = self.maze[x][y]
-        cell.has_visited = True
+        cell = self.maze[x][y] # we get the cell
+        cell.has_visited = True # we set the cell has visited
         return cell
 
     def init_maze(self, width, height) -> None:
@@ -47,6 +50,13 @@ class Maze:
             for y in range(0, height):
                 self.maze[x].append(Cellule(x, y, 0x0F, False))
 
+    def print_hexa_walls(self):
+        width = self.width
+        height = self.height
+        output_file = self.config_data["output_file"]
+
+        with open(output_file, "w") as f:
+            for
     def perfect_maze(self):
         pass
 
